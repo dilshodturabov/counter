cf2730a84ede73093e19291a9bc5945433f9da28
Dilshod <85446738+dilshodturabov@users.noreply.github.com>
2022-05-23 07:43:08 -0700

    counter

---
+++ app.js
@@ -0,0 +1,25 @@
+let count = 0;
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if (styles.contains('decrease')) {
+            count -= 1;
+        } else if(styles.contains('increase')){
+        	count+=1;
+        }else{
+        	count = 0;
+        }
+        if(count < 0){
+        	value.style.color = 'red';
+        }else if(count>0){
+        	value.style.color = 'green';
+        }else{
+        	value.style.color = 'black';
+        }
+        value.textContent = count;
+
+    })
+})
